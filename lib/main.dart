e43225e33f7be5574c8af841d82373cb77e0d611
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-08-19 22:54:01 +0100

    Change order of entries

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String text = "Hello World!";
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 32,
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Training"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("Hello"),
+            Text("World!"),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Button 1"),
+                  onPressed: () {
+                    print("Button 1 pressed.");
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Button 2"),
+                  onPressed: () {
+                    print("Button 2 pressed.");
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Button 3"),
+                  onPressed: () {
+                    print("Button 3 pressed.");
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
