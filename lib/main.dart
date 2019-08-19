b13fecba4039206a917156f1c6021066ef8bdc34
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-08-19 22:02:25 +0100

    Hello World!

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        ),
+      ),
+    );
+  }
+}
