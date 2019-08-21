80a307a2a2acb8253b539dab94f33314f34aa17a
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-08-21 20:09:35 +0100

    Format question widget

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String question;
+  final String question;
 
   Question(this.question);
 
   @override
   Widget build(BuildContext context) {
-    return Text(question);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(8),
+      child: Text(
+        question,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
